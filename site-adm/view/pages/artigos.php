3dcdcdd81233512007fc655028982f878d3554a5
Manoel V. L. de Oliveira <140553447+Tewdric@users.noreply.github.com>
2025-02-24 19:07:31 -0400

    att

---
+++ site-adm/view/pages/artigos.php
@@ -0,0 +1,15 @@
+<?php 
+//require_once __DIR__ . '\..\components\head.php';
+require_once '../components/head.php';
+?>
+<body class="body">
+    <?php require_once '../components/navbar.php'; ?>
+    <?php require_once '../components/sidebar.php'; ?>
+
+    <main class="content-grid">
+        <h1>Artigos</h1>
+    </main>
+
+    <?php require_once '../components/footer.php'; ?>
+</body>
+</html>
